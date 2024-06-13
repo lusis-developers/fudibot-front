7162b59baad033ee17fab13cdd6a5c65c9f41dc5
Jorge DC <jddelcampom@gmail.com>
2024-06-13 17:33:10 -0500

    refactor: Move Wizard page out of app

--- src/views/app/RestaurantInfo/Index.vue
+++ src/views/app/RestaurantInfo/Index.vue
@@ -1,4 +1,24 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+import useClientStore from '@/store/client';
+import useRestaurantStore from '@/store/restaurant';
+
+const restaurantStore = useRestaurantStore();
+const clientStore = useClientStore();
+
+const router = useRouter();
+
+onMounted(async() => {
+  const id = await clientStore.getUserIDRestaurant();
+  if(!id) {
+    router.push('/wizard')
+  } else{
+    const restaurant = await restaurantStore.getRestaurant(id);
+    console.log(restaurant)
+  }
+})
 </script>
 
 <template>
