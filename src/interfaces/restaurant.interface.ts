24ef223a8b4cf4d047a367fc6d189efdff48dd16
Diego Reyes <diegorele13@gmail.com>
2024-06-26 11:42:34 -0500

    update interface according backend

--- src/interfaces/restaurant.interface.ts
+++ src/interfaces/restaurant.interface.ts
@@ -1,23 +1,27 @@
-import { Bank } from './bank.interface';
+import type { Bank } from './bank.interface';
 import type { Coordinates } from './coordinates.interface';
 import type { Drink } from './drink.interface';
 import type { Meal } from './meal.interface';
+import type { PaymentMethods } from './paymentMethods.interface';
 
 export interface Restaurant {
   _id: string,
   botName: string,
   location: Coordinates,
+  uuid: string,
   manager: string,
   email: string,
+  paymentMethods?: PaymentMethods;
   companyName: string,
   phone: string,
   schedule: Schedule[],
   logo: string,
   website: string,
-  uuid: string,
   meals?: Meal[] | string[],
   drinks?: Drink[] | string[], 
   bankSettings?: Bank[]| string[],
+  createdAt: string,
+  updatedAt: string,
 }
 
 export interface Schedule {
