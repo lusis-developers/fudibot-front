fae898b2a112293ac8f342d39921c1870ea02f8f
Diego Reyes <diegorele13@gmail.com>
2024-06-25 22:23:09 -0500

    set: tech lead tips

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,19 +2,18 @@
 
 <template>
   <div class="card">
-    <slot>
-
-    </slot>
+    <slot name="title"></slot>
+    <slot name="content"></slot>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .card {
-  border: 1px solid #ccc;
+  border: 1px solid $grey;
   padding: 16px;
   border-radius: 12px;
-  background-color: #ffffff;
-  color: #333;
+  background-color: $white;
+  color: $dark-grey;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   transition: transform 0.2s, box-shadow 0.2s;
   transform: translateY(-2px);
