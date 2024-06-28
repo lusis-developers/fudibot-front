f7e4de20c5c7e5db97128fc621b2bc4816f31aef
Diego Reyes <diegorele13@gmail.com>
2024-06-28 02:18:47 -0500

    add: service

--- src/services/delivery/delivery.ts
+++ src/services/delivery/delivery.ts
@@ -2,17 +2,26 @@ import { AxiosResponse } from 'axios';
 
 import APIBase from '../base';
 
-import type { Delivery, HasOwnFleet } from '@/interfaces/delivery.interface';
+import type {
+	AddOrEditFleetDetail,
+	Delivery,
+	HasOwnFleet,
+} from '@/interfaces/delivery.interface';
 
 class APIDelivery extends APIBase {
+	async getDeliveryData(id: string): Promise<AxiosResponse> {
+		return await this.get(`/get-delivery-data/${id}`);
+	}
 
-  async getDeliveryData(id: string): Promise<AxiosResponse> {
-    return await this.get(`/get-delivery-data/${id}`);
-  };
+	async postHasOwnFleet(data: HasOwnFleet): Promise<AxiosResponse<Delivery>> {
+		return await this.patch<Delivery>("has-own-fleet", data);
+	}
 
-  async postHasOwnFleet(data: HasOwnFleet): Promise<AxiosResponse<Delivery>> {
-    return await this.patch<Delivery>('has-own-fleet', data);
-  };
-};
+	async addFleetDetail(
+		data: AddOrEditFleetDetail
+	): Promise<AxiosResponse<Delivery>> {
+		return await this.post<Delivery>("add-fleet-detail", data);
+	}
+}
 
 export default APIDelivery;
